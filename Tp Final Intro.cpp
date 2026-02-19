35e256711c81170fd9c4562ebc349393f756fc4e
Pedro <pedrogbazan@live.com>
2026-02-19 04:53:01 -0300

    Primer archivo, inicio con la estructura

---
+++ Tp Final Intro.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio2.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	cout<< "Hola me llamo Pedro, hoy comenzamos a full!";
+	
+	/* voy a necesitar:
+	main
+	
+	
+	
+	Clase base para objetos.
+	
+	Clase Enemigo
+			Clase enemigo simple
+			Clase enemigo fuerte
+	
+	Clase jugador
+	
+	clase Bala
+	
+	Pantalla Inicio
+	
+	Mecanicas
+	*/
+	
+	return 0;
+}
+
